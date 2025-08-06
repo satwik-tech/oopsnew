c07e69edb9817d228b0d4d5e59c7bb0345f39737
sathwik 24020030048 <sathwikmintu03@gmail.com>
2025-08-06 12:20:47 +0530

    Add files via upload

---
+++ stringbuffer.java
@@ -0,0 +1,13 @@
+package strings;
+
+public class stringbuffer {
+	public static void main(String[] args) {
+		StringBuffer x= new StringBuffer("Hello! ");
+		x.append("JAVA");
+		System.out.println(x);
+		
+
+	}
+
+
+}
